a3989101e7093dc35fa9c8b000be972d0b0ab8e5
Giovanni Stephens <stephens.giovanni@gmail.com>
2020-12-10 17:02:51 +1300

    Added more tests to see if the clustering is working.

--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -22,24 +22,24 @@ class test_embeddings(unittest.TestCase):
             sumxy += x*y
         return sumxy/(sumxx*sumyy)**0.5
 
+    def _get_reduced_embeddings(self):
+        embed = self._get_module('embeddings')
+        clustering =  self._get_module('clustering')
+        embeddings = embed.pretrained_transformer_embeddings(TEST_UTTERANCES)
+        n_dimensions = 2
+        reduced = clustering.reduce_dimensions_pca(embeddings, n_dimensions)
+        return reduced
+
     def test_import_embeddings_module(self):
         embed = self._get_module('embeddings')
         self.assertIsNotNone(embed)
 
     def test_reduce_dimensions_pca_num_dimensions(self):
-        embed = self._get_module('embeddings')
-        clustering =  self._get_module('clustering')
-        embeddings = embed.pretrained_transformer_embeddings(TEST_UTTERANCES)
-        n_dimensions = 3
-        reduced = clustering.reduce_dimensions_pca(embeddings, n_dimensions)
-        self.assertEqual(len(reduced[0]), n_dimensions)
+        reduced = self._get_reduced_embeddings()
+        self.assertEqual(len(reduced[0]), 2)
 
     def test_reduce_dimensions_pca_similarity(self):
-        embed = self._get_module('embeddings')
-        clustering =  self._get_module('clustering')
-        embeddings = embed.pretrained_transformer_embeddings(TEST_UTTERANCES)
-        n_dimensions = 3
-        reduced = clustering.reduce_dimensions_pca(embeddings, n_dimensions)
+        reduced = self._get_reduced_embeddings()
         sim_1 = self._cosine_similarity(reduced[0], reduced[1])
         sim_2 = self._cosine_similarity(reduced[0], reduced[2])
         self.assertGreater(sim_1, sim_2)
@@ -59,7 +59,29 @@ class test_embeddings(unittest.TestCase):
         n_dimensions = clustering.get_optimal_n_components(embeddings)
         self.assertEqual(n_dimensions, 2)
 
+    def test_kmeans_clustering_diff_cluster(self):
+        clustering =  self._get_module('clustering')
+        reduced = self._get_reduced_embeddings()
+        cluster_labels = clustering.kmeans_clustering(reduced, max_num_clusters = 3)
+        self.assertNotEqual(cluster_labels[0], cluster_labels[2])
 
+    def test_hdbscan_clustering_diff_cluster(self):
+        clustering =  self._get_module('clustering')
+        reduced = self._get_reduced_embeddings()
+        cluster_labels = clustering.hdbscan_clustering(reduced, min_cluster_size = 2)
+        self.assertEqual(cluster_labels[0], cluster_labels[2])
+
+    def test_kmeans_clustering_same_cluster(self):
+        clustering =  self._get_module('clustering')
+        reduced = self._get_reduced_embeddings()
+        cluster_labels = clustering.kmeans_clustering(reduced, max_num_clusters = 3)
+        self.assertEqual(cluster_labels[0], cluster_labels[1])
+
+    def test_hdbscan_clustering_same_cluster(self):
+        clustering =  self._get_module('clustering')
+        reduced = self._get_reduced_embeddings()
+        cluster_labels = clustering.hdbscan_clustering(reduced, min_cluster_size = 2)
+        self.assertEqual(cluster_labels[0], cluster_labels[1])
 
 if __name__ == '__main__':
     unittest.main()
