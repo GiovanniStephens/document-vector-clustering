2c2269269c8c4b1cfa5d4001795a1455a8b82722
Giovanni Stephens <stephens.giovanni@gmail.com>
2020-12-10 17:08:14 +1300

    Added comments.

--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -23,6 +23,8 @@ class test_embeddings(unittest.TestCase):
         return sumxy/(sumxx*sumyy)**0.5
 
     def _get_reduced_embeddings(self):
+        """Imports embeddings module and reduces the sentence vectors
+        to two dimensions."""
         embed = self._get_module('embeddings')
         clustering =  self._get_module('clustering')
         embeddings = embed.pretrained_transformer_embeddings(TEST_UTTERANCES)
@@ -31,20 +33,32 @@ class test_embeddings(unittest.TestCase):
         return reduced
 
     def test_import_embeddings_module(self):
+        """Tests that you can import the embeddings module."""
         embed = self._get_module('embeddings')
         self.assertIsNotNone(embed)
 
+    def test_import_clustering_module(self):
+        """Tests that you can import the clustering module."""
+        embed = self._get_module('clustering')
+        self.assertIsNotNone(embed)
+
     def test_reduce_dimensions_pca_num_dimensions(self):
+        """Tests the size of the vector after dimension reduction using PCA."""
         reduced = self._get_reduced_embeddings()
         self.assertEqual(len(reduced[0]), 2)
 
     def test_reduce_dimensions_pca_similarity(self):
+        """Tests vector similarity by ways of cosine similarity 
+        using the reduced vectors.
+        The first two test phrases should be more similar than
+        the first and third."""
         reduced = self._get_reduced_embeddings()
         sim_1 = self._cosine_similarity(reduced[0], reduced[1])
         sim_2 = self._cosine_similarity(reduced[0], reduced[2])
         self.assertGreater(sim_1, sim_2)
 
     def test_reduce_dimensions_umap(self):
+        """Tests the size of the vector after dimension reduction using UMAP."""
         embed = self._get_module('embeddings')
         clustering =  self._get_module('clustering')
         embeddings = embed.pretrained_transformer_embeddings(TEST_UTTERANCES*10)
@@ -53,6 +67,8 @@ class test_embeddings(unittest.TestCase):
         self.assertEqual(len(reduced[0]), n_dimensions)
     
     def test_get_optimal_n_components(self):
+        """Tests the function to find the optimal number of principal
+        components to keep."""
         embed = self._get_module('embeddings')
         clustering =  self._get_module('clustering')
         embeddings = embed.pretrained_transformer_embeddings(TEST_UTTERANCES*10)
@@ -60,24 +76,28 @@ class test_embeddings(unittest.TestCase):
         self.assertEqual(n_dimensions, 2)
 
     def test_kmeans_clustering_diff_cluster(self):
+        """Tests that the first phrase is not clustered with the third using kmeans."""
         clustering =  self._get_module('clustering')
         reduced = self._get_reduced_embeddings()
         cluster_labels = clustering.kmeans_clustering(reduced, max_num_clusters = 3)
         self.assertNotEqual(cluster_labels[0], cluster_labels[2])
 
     def test_hdbscan_clustering_diff_cluster(self):
+        """Tests that the first phrase is not clustered with the third using HDBSCAN."""
         clustering =  self._get_module('clustering')
         reduced = self._get_reduced_embeddings()
         cluster_labels = clustering.hdbscan_clustering(reduced, min_cluster_size = 2)
         self.assertEqual(cluster_labels[0], cluster_labels[2])
 
     def test_kmeans_clustering_same_cluster(self):
+        """Tests that the first phrase is clustered with the second using kmeans."""
         clustering =  self._get_module('clustering')
         reduced = self._get_reduced_embeddings()
         cluster_labels = clustering.kmeans_clustering(reduced, max_num_clusters = 3)
         self.assertEqual(cluster_labels[0], cluster_labels[1])
 
     def test_hdbscan_clustering_same_cluster(self):
+        """Tests that the first phrase is clustered with the second using HDBSCAN."""
         clustering =  self._get_module('clustering')
         reduced = self._get_reduced_embeddings()
         cluster_labels = clustering.hdbscan_clustering(reduced, min_cluster_size = 2)
